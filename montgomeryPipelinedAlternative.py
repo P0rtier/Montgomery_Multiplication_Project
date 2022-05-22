afb7f5cf3afbe8c7dc92214b3b995d1fc634a71b
P0rtier <patrykw595@gmail.com>
2022-05-22 19:43:17 +0200

    program properly counting coefficients G,F and E

--- montgomeryPipelinedAlternative.py
+++ montgomeryPipelinedAlternative.py
@@ -22,6 +22,11 @@ def monPro(a, b, r, n, bit_width):
     else:
         return s
 
+def monProM(a, b, r, n, bit_width):
+    t = a * b
+    m = ((t % r) * neg_inv(n, bit_width, r)) % r
+    return m
+
 
 def modMul(x, y, n):
     bit_width = calc_r(n)
@@ -139,19 +144,73 @@ def alternativeModularMultiplication2(e2,e3,x,y):
     n_temp = give_newY(n,2*e2,k+2)
     G = t_temp * n_temp
     MP = pow(2,2*e2) * F + pow(2,2*e2) * G + pow(2,e2) * E - m
-    S = (t + MP)/r
+    m = monProM(x,y,r,n,bit_width)
+    S = (t + m*n)/r
     return S
 
-e2 = 4
-e3 = 3
+def alternativeModularMultiplication3(e2,e3,x,y):
+    n = (pow(2,e2) * pow(3,e3)) - 1
+    bit_width = calc_r(n)
+    r = 2**calc_r(n)
+    x = x * r % n
+    y = y * r % n
+    k = e2 + math.ceil(math.log2(pow(3,e3)))
+    t = x*y
+    n_prim = neg_invAlternate(n,k,e2,e3)
+    #n_prim = neg_inv(n,bit_width,r)
+    #C:
+    t_temp = give_newY(t,e2,k+2-e2)
+    n_prim_temp = give_newY(n_prim,e2,k+2-e2)
+    C = t_temp * n_prim_temp
+    #D:
+    t_temp = give_newY(t,0,k+2-(2*e2))
+    n_prim_temp = give_newY(n_prim,2*e2,k+2)
+    D = t_temp * n_prim_temp
+
+
+    #E:
+    t_temp = give_newY(t,0,e2)
+    n_prim_temp = give_newY(n_prim,e2,2*e2)
+    E = t_temp * n_prim_temp
+
+    #m1:
+    m = (pow(2,2*e2)*((C+D) % pow(2,k+2-(2*e2))) + pow(2,e2) * give_newY(t,e2,k+2)
+         + pow(2,e2)*E) % pow(2,k+2) + give_newY(t,0,e2)
+
+    m = monProM(x, y, r, n, bit_width)
+    m = pow(2,e2) * give_newY(m,e2,k+2) + give_newY(t,0,e2)
+    #F:
+    m_temp = give_newY(m,e2,k+2)
+    n_temp = give_newY(n,2*e2,k+2)
+    n_prim_temp = give_newY(n_prim,e2,2*e2)
+    F = m_temp * (pow(2,e2) * n_temp + n_prim_temp)
+    #G:
+    t_temp = give_newY(t,0,e2)
+    n_temp = give_newY(n,2*e2,k+2)
+    G = t_temp * n_temp
+    MP = pow(2,2*e2) * F + pow(2,2*e2) * G + pow(2,e2) * E - m
+    #m = monProM(x, y, r, n, bit_width)
+    #m = pow(2,e2)*give_newY(m,e2,k+2) + give_newY(t,0,e2)
+    s = (t + MP) / r
+    if s >= n:
+        return s - n
+    else:
+        return s
+
+e2 = 3
+e3 = 2
 n = (pow(2,e2) * pow(3,e3)) - 1
-x = 7
-y = 21
+x = 11
+y = 12
 xy = x*y
+bit_width = calc_r(n)
+r = 2 ** calc_r(n)
 print('Oczekiwana:',xy % n)
 
 print('Montg:',modMul(x,y,n))
-print('montgomery:',int(alternativeModularMultiplication2(e2,e3,x,y)))
+
+mid_result = (alternativeModularMultiplication3(e2,e3,x,y))
+print('Montg',int(monPro(mid_result, 1, r, n, bit_width)))
 
 
 # Start:
